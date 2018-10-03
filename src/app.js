cbd67e7e870401ab9fb11c10fe16591ca88164ab
wjxalexander <wjxalexander@hotmail.com>
2018-10-03 23:58:40 +0800

    mock那个没有用

--- src/app.js
+++ src/app.js
@@ -15,3 +15,92 @@ var app = new Vue({
     loading3: false,
   }
 })
+
+//单元测试
+import chai from'chai'
+const expect = chai.expect
+{
+  //由于button是一个对象，没法通过对象实例化，所以将对象改成函数
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon:"setting",
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use');
+  console.log(useElement)
+  let href = useElement.getAttribute('xlink:href')
+  console.log(href)
+  expect(href).to.eq('#i-setting')
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon:"setting",
+      loading:true,
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use');
+  console.log(useElement)
+  let href = useElement.getAttribute('xlink:href')
+  console.log(href)
+  expect(href).to.eq('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      loading:true,
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use');
+  console.log(useElement)
+  let href = useElement.getAttribute('xlink:href')
+  console.log(href)
+  expect(href).to.eq('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  // mock
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+  // 希望这个函数被执行
+  let button = vm.$el
+  button.click()
+}
+
+
+  // {
+  //   const div = document.createElement('div')
+  //   document.body.appendChild(div)
+  //   const Constructor = Vue.extend(Button)
+  //   const vm = new Constructor({
+  //     propsData:{
+  //       icon:"setting",
+  //       iconPosition: "left"
+  //     }
+  //   })
+  //   vm.$mount("div")
+  //   let svg = vm.$el.querySelector('svg');
+
+  //   console.log(svg)
+  //   let {order} = window.getComputedStyle(svg)
+  //   expect(order).to.eq("1")
+  // vm.$el.remove()
+  // vm.$destroy()
+  // }
