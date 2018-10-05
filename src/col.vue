2d686756c7fae0eaf5d7b2390950fa79a714c3b6
wjxalexander <wjxalexander@hotmail.com>
2018-10-05 14:58:54 +0800

    栅格初步

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[`col-${span}`,offset&&`offset-${offset}`]"
+  :style="{paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}">
     <slot> </slot>
   </div>
 </template>
@@ -9,29 +10,49 @@ export default {
   props:{
     span: {
       type:[Number,String]
+    },
+    offset:{
+      type:[Number,String]
+    },
+  },
+  data(){
+    return {
+      gutter: 0
     }
-  } 
+  },
+  created(){
+    console.log('col crted')
+  },
+  mounted(){
+    console.log('col mount ')
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .col{
-    height: 100px;
-    background:#cccc;
-    width: 50%;
-    border: 1px solid red;
-  }
+  .col{ height: 100px; background:#cccc; width: 50%; border: 1px solid red; 
     $class-prefix: col-;
   //说明class前缀
     @for $n from 1 through 24{
     //.col.col-1
     //.col.col-2
     //.col.col-3
-      .#{$class-prefix}#{$n}{
+      &.#{$class-prefix}#{$n}{
       //col-n
         width: ($n / 24) * 100%
        }
     }
+    $class-prefix: offset-;
+    @for $n from 1 through 24{
+    //.col.col-1
+    //.col.col-2
+    //.col.col-3
+    &.#{$class-prefix}#{$n}{
+      //col-n
+        margin-left: ($n / 24) * 100%
+       }
+    }
+}
   
 </style>
 
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,32 @@
-<template>
-  <div class="row">
+<template>  
+  <div class="row" :style="{marginLeft:-gutter/2+'px', marginRight:-gutter/2+'px',}">
     <slot></slot>
   </div>  
 </template>
+<script>
+export default {
+  name: "g-row",
+  props:{
+    gutter:{
+      type:[String,Number],
+      default:0
+    }
+  },
+  created(){
+    console.log('row cretd') 
+    //这时没有儿子，
+  },
+  mounted(){
+    console.log('row mounted') 
+    //放在DOM里了
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter;
+      //获取子元素传gutter
+    })
+  }
+}
+</script>
+
 <style lang="scss" scoped>
   .row{
     display: flex;
