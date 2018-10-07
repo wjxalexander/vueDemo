9f2a9344602b42e26c201c7987a1a281127d5e38
wjxalexander <wjxalexander@hotmail.com>
2018-10-07 22:15:33 +0800

    没有解决点击pop over的隐藏问题

--- src/popover.vue
+++ src/popover.vue
@@ -8,33 +8,41 @@
 </template>
 <script>
 export default {
-  name:'popover',
-  data(){
+  name: "popover",
+  data() {
     return {
-      visible: false,
-    }
+      visible: false
+    };
   },
-  methods:{
-    changeState(){
-      this.visible = !this.visible
+  methods: {
+    changeState() {
+      let _this = this;
+      console.log("visble change1");
+      this.visible = !this.visible;
+      setTimeout(() => {
+        let eventHandler = () =>{
+          this.visible =false
+          document.removeEventListener('click',eventHandler)
+        }
+        document.addEventListener('click',eventHandler)
+      }, 1000);
     }
   }
-}
+};
 </script>
 
 
 <style lang="scss" scoped>
-.popover{
+.popover {
   display: inline-block;
-  vertical-align: top;  
+  vertical-align: top;
   position: relative;
-  > .content-wrapper{ 
+  > .content-wrapper {
     position: absolute;
     bottom: 100%;
     left: 0;
     background: #ccc;
-    border: 1px solid red
-
-}
+    border: 1px solid red;
+  }
 }
 </style>
