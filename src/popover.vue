bdf88e3fe6c385bfd9e187698852deab95232bf8
wjxalexander <wjxalexander@hotmail.com>
2018-10-13 11:26:15 +0800

    功能基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class= 'popover' @click.stop="changeState">
-    <div ref='contentwrap' class="content-wrapper" v-if="visible" @click.stop>
+  <div class= 'popover' @click="changeState" ref="popover">
+    <div ref='contentwrap' class="content-wrapper" v-if="visible" >
       <slot name='content'></slot>
     </div>
     <span ref='triggerwrap'>
@@ -14,32 +14,47 @@ export default {
   data() {
     return {
       visible: false
-    }
+    };
   },
   methods: {
-    changeState() {
-      console.log(this.visible);
-      this.visible = !this.visible;
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentwrap)
-          let {width,height,top, left}=this.$refs.triggerwrap.getBoundingClientRect()
-          this.$refs.contentwrap.style.left = left + window.scrollX+ 'px'
-          this.$refs.contentwrap.style.top = top + window.scrollY+ 'px'
-          let eventHandler = () => {
-            this.visible = false;
-            console.log("document隐藏");
-            document.removeEventListener("click", eventHandler);
-          };
-          document.addEventListener("click", eventHandler);
+    postionContent() {
+      document.body.appendChild(this.$refs.contentwrap);
+      let {width, height, top, left} = this.$refs.triggerwrap.getBoundingClientRect();
+      this.$refs.contentwrap.style.left = left + window.scrollX + "px";
+      this.$refs.contentwrap.style.top = top + window.scrollY + "px";
+    },
+    ListenToDoc() {
+      let eventHandler = e => {
+        if (this.$refs.contentwrap&&!this.$refs.contentwrap.contains(e.target)) {
+          this.close(eventHandler)
+        }
+      };
+      document.addEventListener("click", eventHandler);
+    },
+    close(node){
+      this.visible = false
+      document.removeEventListener("click", node);
+
+    },
+    onOpen(){
+      this.visible = true;
+      this.$nextTick(() => {
+          this.postionContent();
+          this.ListenToDoc();
         });
-      } else {
-        console.log("vm隐藏");
-      }
+  },
+    changeState(event) {
+      if (this.$refs.triggerwrap.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        }else{
+         this.onOpen()
+        }
+      } 
     }
   },
-  mounted(){
-     console.log(this.$refs.contentwrap)
+  mounted() {
+    console.log(this.$refs.contentwrap);
   }
 };
 </script>
@@ -52,9 +67,9 @@ export default {
   position: relative;
 }
 .content-wrapper {
-    position: absolute;
-    background: #ccc;
-    border: 1px solid red;
-    transform:translateY(-100%)
-  }
+  position: absolute;
+  background: #ccc;
+  border: 1px solid red;
+  transform: translateY(-100%);
+}
 </style>
